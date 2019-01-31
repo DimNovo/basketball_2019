864ccbc6fd5645e0e33ce0d9d92c46018a0be3c4
Dmitry Novosyolov <i1710@icloud.com>
2019-01-31 12:28:53 +0200

    / 1:45:33 on Lesson's video /

--- basketball/ViewController.swift
+++ basketball/ViewController.swift
@@ -9,7 +9,10 @@
 import ARKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - ... Properties
+    var hoopAdded = false
+    
     // MARK: - ... @IBOutlet
     @IBOutlet var sceneView: ARSCNView!
     
@@ -39,7 +42,7 @@ class ViewController: UIViewController {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = [.vertical]
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -52,14 +55,37 @@ class ViewController: UIViewController {
     }
     
     // MARK: - ... Custom Methods
-    func createNode (from sceneName: String) -> SCNNode? {
-        guard let scene = SCNScene(named: "art.scnassets/\(sceneName)") else {
+    func createBasketBall() {
+        
+        guard let ballNode = createNode(from: "Ball.scn") else { return }
+        guard let frame = sceneView.session.currentFrame else { return }
+        ballNode.simdTransform = frame.camera.transform
+        
+        sceneView.scene.rootNode.addChildNode(ballNode)
+    }
+    
+    func createHoop(result: ARHitTestResult) {
+        
+        guard let hoopNode = createNode(from: "Hoop.scn") else { return }
+        hoopNode.simdTransform = result.worldTransform
+        hoopNode.eulerAngles.x -= .pi / 2
+        
+        hoopAdded = true
+        stopPlaneDetection()
+        removeWalls()
+        
+        sceneView.scene.rootNode.addChildNode(hoopNode)
+    }
+    
+    func createNode(from sceneName: String) -> SCNNode? {
+        
+        guard let scene = SCNScene(named: "art.scnassets/\(sceneName)") else
+        {
             print(#function, "ERROR: Can't create node from scene \(sceneName)")
             return nil
         }
         
         let node = scene.rootNode.clone()
-        
         
         return node
     }
@@ -74,27 +100,49 @@ class ViewController: UIViewController {
         
         node.eulerAngles.x -= .pi / 2
         node.geometry?.firstMaterial?.diffuse.contents = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        node.name = "Wall"
         node.opacity = 0.25
         
         return node
     }
     
+    func removeWalls() {
+        
+        sceneView.scene.rootNode.enumerateChildNodes { node,_ in
+            guard node.name != "Wall" else {
+                removeFromParent()
+                return
+            }
+        }
+    }
+    
+    func stopPlaneDetection() {
+        
+        guard let configuration = sceneView.session.configuration as? ARWorldTrackingConfiguration else { return }
+        
+        configuration.planeDetection = []
+        sceneView.session.run(configuration)
+    }
+    
+    // MARK: - ... @IBAction
     @IBAction func screenTapped(_ sender: UITapGestureRecognizer) {
+        
+        guard !hoopAdded else {
+            createBasketBall()
+            return
+        }
+        
         let location = sender.location(in: sceneView)
-        
         guard let result = sceneView.hitTest(location, types: [.existingPlaneUsingExtent]).first else { return }
-        
-        print(#function, Date())
+        createHoop(result: result)
     }
 }
 
 // MARK: - ... ARSCNViewDelegate
 extension ViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        print(#function)
         
         guard let anchor = anchor as? ARPlaneAnchor else { return }
-        
         let wall = createWall(anchor: anchor)
         
         node.addChildNode(wall)
